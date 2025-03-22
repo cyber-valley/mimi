695365af93ea98ec2ff5613786dd1138dd1dd73b
scipunch <scipunch@gmail.com>
2025-03-22 13:03:25 +0100

    refactor: Format

--- mimi/data_scraper/__init__.py
+++ mimi/data_scraper/__init__.py
@@ -1,7 +1,7 @@
-from multiprocessing.pool import ThreadPool
 from collections.abc import Awaitable, Callable, Iterable
 from dataclasses import dataclass
 from datetime import datetime
+from multiprocessing.pool import ThreadPool
 from typing import NoReturn
 
 from mimi import DataOrigin, DataSink
@@ -24,7 +24,7 @@ DataScraper = Callable[[DataSink[DataScraperMessage]], Awaitable[NoReturn]]
 def run_scrapers(
     pool: ThreadPool,
     sink: DataSink[DataScraperMessage],
-    scrapers: Iterable[DataScraper]
+    scrapers: Iterable[DataScraper],
 ) -> NoReturn:
     for scraper in scrapers:
         pool.apply_async(scraper, (sink,))
