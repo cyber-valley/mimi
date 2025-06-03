1a949d9d9637ae14bbbfc651b94757225906ae59
scipunch <scipunch@gmail.com>
2025-06-03 23:09:45 +0200

    feat: Login into telegram account & fetch info

--- cmd/scraper/telegram.go
+++ cmd/scraper/telegram.go
@@ -1,115 +1,167 @@
-// Binary bot-auth-manual implements example of custom session storage and
-// manually setting up client options without environment variables.
 package main
 
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
+	"os/signal"
+	"path/filepath"
 	"strconv"
-	"sync"
+	"time"
 
+	tgscraper "mimi/internal/scraper/telegram"
+
+	"github.com/go-faster/errors"
 	"github.com/golang/glog"
+	"github.com/gotd/contrib/middleware/floodwait"
+	"github.com/gotd/contrib/middleware/ratelimit"
+	"golang.org/x/time/rate"
 
-	"github.com/gotd/td/session"
 	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/auth"
+	"github.com/gotd/td/tg"
 )
+
+func sessionFolder(phone string) string {
+	var out []rune
+	for _, r := range phone {
+		if r >= '0' && r <= '9' {
+			out = append(out, r)
+		}
+	}
+	return "phone-" + string(out)
+}
 
 const (
-	telegramAppID    = "TELEGRAM_APP_ID"
-	telegramAppHash  = "TELEGRAM_APP_HASH"
-	telegramBotToken = "TELEGRAM_BOT_TOKEN"
+	tgAppID    = "TG_APP_ID"
+	tgAppHash  = "TG_APP_HASH"
+	tgBotToken = "TG_BOT_TOKEN"
+	tgPhone    = "TG_PHONE"
 )
 
-// memorySession implements in-memory session storage.
-// Goroutine-safe.
-type memorySession struct {
-	mux  sync.RWMutex
-	data []byte
-}
+func run(ctx context.Context) error {
+	flag.Lookup("stderrthreshold").Value.Set("INFO")
+	flag.Parse()
 
-// LoadSession loads session from memory.
-func (s *memorySession) LoadSession(context.Context) ([]byte, error) {
-	if s == nil {
-		return nil, session.ErrNotFound
+	phone := os.Getenv(tgPhone)
+	if phone == "" {
+		return errors.New("no phone")
+	}
+	// APP_HASH, APP_ID is from https://my.telegram.org/.
+	appID, err := strconv.Atoi(os.Getenv(tgAppID))
+	if err != nil {
+		return errors.Wrap(err, " parse app id")
+	}
+	appHash := os.Getenv(tgAppHash)
+	if appHash == "" {
+		return errors.New("no app hash")
 	}
 
-	s.mux.RLock()
-	defer s.mux.RUnlock()
-
-	if len(s.data) == 0 {
-		return nil, session.ErrNotFound
+	// Setting up session storage.
+	// This is needed to reuse session and not login every time.
+	sessionDir := filepath.Join("session", sessionFolder(phone))
+	if err := os.MkdirAll(sessionDir, 0700); err != nil {
+		return err
 	}
 
-	cpy := append([]byte(nil), s.data...)
+	glog.Infof("Storing session in %s", sessionDir)
 
-	return cpy, nil
-}
+	// So, we are storing session information in current directory, under subdirectory "session/phone_hash"
+	sessionStorage := &telegram.FileSessionStorage{
+		Path: filepath.Join(sessionDir, "session.json"),
+	}
 
-// StoreSession stores session to memory.
-func (s *memorySession) StoreSession(ctx context.Context, data []byte) error {
-	s.mux.Lock()
-	s.data = data
-	s.mux.Unlock()
-	return nil
+	// Setting up client.
+	//
+	// Dispatcher is used to register handlers for events.
+	dispatcher := tg.NewUpdateDispatcher()
+
+	// Handler of FLOOD_WAIT that will automatically retry request.
+	waiter := floodwait.NewWaiter().WithCallback(func(ctx context.Context, wait floodwait.FloodWait) {
+		// Notifying about flood wait.
+		glog.Info("Got FLOOD_WAIT. Will retry after", wait.Duration)
+	})
+
+	// Filling client options.
+	options := telegram.Options{
+		SessionStorage: sessionStorage, // Setting up session sessionStorage to store auth data.
+		Middlewares: []telegram.Middleware{
+			// Setting up FLOOD_WAIT handler to automatically wait and retry request.
+			waiter,
+			// Setting up general rate limits to less likely get flood wait errors.
+			ratelimit.New(rate.Every(time.Millisecond*100), 5),
+		},
+	}
+	client := telegram.NewClient(appID, appHash, options)
+	// api := client.API()
+
+	// Registering handler for new private messages.
+	dispatcher.OnNewMessage(func(ctx context.Context, e tg.Entities, u *tg.UpdateNewMessage) error {
+		msg, ok := u.Message.(*tg.Message)
+		if !ok {
+			return nil
+		}
+		if msg.Out {
+			// Outgoing message.
+			return nil
+		}
+
+		glog.Info("new message: %s", msg.Message)
+		return nil
+	})
+
+	// Authentication flow handles authentication process, like prompting for code and 2FA password.
+	flow := auth.NewFlow(tgscraper.Terminal{PhoneNumber: phone}, auth.SendCodeOptions{})
+
+	return waiter.Run(ctx, func(ctx context.Context) error {
+		// Spawning main goroutine.
+		if err := client.Run(ctx, func(ctx context.Context) error {
+			// Perform auth if no session is available.
+			if err := client.Auth().IfNecessary(ctx, flow); err != nil {
+				return errors.Wrap(err, "auth")
+			}
+
+			// Getting info about current user.
+			self, err := client.Self(ctx)
+			if err != nil {
+				return errors.Wrap(err, "call self")
+			}
+
+			name := self.FirstName
+			if self.Username != "" {
+				// Username is optional.
+				name = fmt.Sprintf("%s (@%s)", name, self.Username)
+			}
+			fmt.Println("Current user:", name)
+
+			glog.Infof("Login firstName=%s, lastName=%s, username=%s, id=%s",
+				self.FirstName,
+				self.LastName,
+				self.Username,
+				self.ID,
+			)
+			return nil
+		}); err != nil {
+			return errors.Wrap(err, "run")
+		}
+		return nil
+	})
 }
 
 func main() {
-	// Grab those from https://my.telegram.org/apps.
-	flag.Parse()
-	flag.Lookup("stderrthreshold").Value.Set("INFO")
-	var missingVars []string
-	appID, err := strconv.Atoi(os.Getenv(telegramAppID))
-	if err != nil {
-		missingVars = append(missingVars, telegramAppID)
-	}
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
 
-	appHash := os.Getenv(telegramAppHash)
-	if appHash == "" {
-		missingVars = append(missingVars, telegramAppHash)
-	}
-
-	botToken := os.Getenv(telegramBotToken)
-	if botToken == "" {
-		missingVars = append(missingVars, telegramBotToken)
-	}
-
-	if missingVars != nil {
-		glog.Fatalf("Missing the following ENV variables: %v", missingVars)
-	}
-
-	// Using custom session storage.
-	// You can save session to database, e.g. Redis, MongoDB or postgres.
-	// See memorySession for implementation details.
-	sessionStorage := &memorySession{}
-	ctx := context.Background()
-
-	client := telegram.NewClient(appID, appHash, telegram.Options{
-		SessionStorage: sessionStorage,
-	})
-
-	if err := client.Run(ctx, func(ctx context.Context) error {
-		// Checking auth status.
-		status, err := client.Auth().Status(ctx)
-		if err != nil {
-
-			return err
+	if err := run(ctx); err != nil {
+		if errors.Is(err, context.Canceled) && ctx.Err() == context.Canceled {
+			fmt.Println("\rClosed")
+			os.Exit(0)
 		}
-		// Can be already authenticated if we have valid session in
-		// session storage.
-		if !status.Authorized {
-			// Otherwise, perform bot authentication.
-			if _, err := client.Auth().Bot(ctx, botToken); err != nil {
-				return err
-			}
-		}
-
-		// All good, manually authenticated.
-		glog.Info("Done")
-
-		return nil
-	}); err != nil {
-		glog.Fatalf("Failed to auth with %s", err)
+		_, _ = fmt.Fprintf(os.Stderr, "Error: %+v\n", err)
+		os.Exit(1)
+	} else {
+		fmt.Println("Done")
+		os.Exit(0)
 	}
-
 }
