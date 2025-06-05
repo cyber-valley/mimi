b6d35a1650e6c4e659fd176db7f61f49ef8eb290
scipunch <scipunch@gmail.com>
2025-06-05 20:12:35 +0200

    refactor: Format sql schema file

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,8 +1,10 @@
 CREATE EXTENSION vector;
 
 CREATE TABLE embedding (
-       id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-       text TEXT NOT NULL,
-       metadata JSONB NOT NULL,
-       embedding vector(1536)
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  text TEXT NOT NULL,
+  metadata JSONB NOT NULL,
+  embedding vector(1536)
+);
+
 );
