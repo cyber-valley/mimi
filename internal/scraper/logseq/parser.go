8d5098a8cde9891dfff46ed83fe0e99356beec1b
scipunch <scipunch@gmail.com>
2025-06-08 18:12:10 +0200

    feat: Add basic Logseq graph loading and page listing

--- internal/scraper/logseq/parser.go
+++ internal/scraper/logseq/parser.go
@@ -11,11 +11,24 @@ import (
 )
 
 func SyncGraph(ctx context.Context, path string) error {
-	graph, err := logseq.Open(ctx, path)
+	g, err := logseq.Open(ctx, path, logseq.WithInMemoryIndex())
 	if err != nil {
 		glog.Errorf("failed to open graph %s with: %s", path, err)
 		return err
 	}
-	glog.Infof("graph: %#v", graph)
+	glog.Infof("graph: %#v", g)
+	pages, err := g.SearchPages(ctx)
+	if err != nil {
+		glog.Errorf("failed to search pages with: %s", err)
+		return err
+	}
+	for _, p := range pages.Results() {
+		glog.Infof("page: %s", p.Title())
+		p, err := p.Open()
+		if err != nil {
+			glog.Warning("failed to open page with: %s", err)
+		}
+		glog.Infof("subpath: %T %#v", p, p)
+	}
 	return nil
 }
