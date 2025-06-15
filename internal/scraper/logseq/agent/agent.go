842fffe33cbfbdbd466dc2d8fb1c623ebafd90c6
scipunch <scipunch@gmail.com>
2025-06-15 13:51:53 +0200

    refactor: Decompose logseq retriever into own package

--- internal/scraper/logseq/agent/agent.go
+++ internal/scraper/logseq/agent/agent.go
@@ -3,23 +3,19 @@ package agent
 import (
 	"context"
 	"log"
-	"os"
 
-	"github.com/firebase/genkit/go/ai"
 	"github.com/firebase/genkit/go/genkit"
-	"github.com/firebase/genkit/go/plugins/compat_oai/openai"
 	"github.com/firebase/genkit/go/plugins/googlegenai"
 
-	"mimi/internal/scraper/logseq/db"
+	"mimi/internal/scraper/logseq/rag"
 )
 
 type LogseqAgent struct {
-	g *genkit.Genkit
-	q *db.Queries
-	e ai.Embedder
+	g   *genkit.Genkit
+	rag rag.RAG
 }
 
-func New(ctx context.Context, q *db.Queries) LogseqAgent {
+func New(ctx context.Context, rag rag.RAG) LogseqAgent {
 	// Init genkit
 	g, err := genkit.Init(
 		ctx,
@@ -36,27 +32,10 @@ func New(ctx context.Context, q *db.Queries) LogseqAgent {
 		log.Fatal("no prompt named 'menu' found")
 	}
 
-	// Setup OpenAI embedder
-	oaiKey := os.Getenv("OPENAI_API_TOKEN")
-	if oaiKey == "" {
-		log.Fatal("OPENAI_API_TOKEN env var should be set")
-	}
-	oai := openai.OpenAI{
-		APIKey: oaiKey,
-	}
-	if err = oai.Init(ctx, g); err != nil {
-		log.Fatalf("failed to init OpenAI plugin with %s", err)
-	}
-	embedder, err := oai.DefineEmbedder(g, "text-embedding-3-small")
-	if err != nil {
-		log.Fatalf("failed to define OpenAI embedder with %s", err)
-	}
-
 	// Done
 	return LogseqAgent{
-		g: g,
-		q: q,
-		e: embedder,
+		g:   g,
+		rag: rag,
 	}
 }
 
---
+++ internal/scraper/logseq/rag/rag.go
@@ -0,0 +1,64 @@
+package rag
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/firebase/genkit/go/ai"
+	"github.com/firebase/genkit/go/genkit"
+	"github.com/firebase/genkit/go/plugins/compat_oai/openai"
+
+	"mimi/internal/scraper/logseq/db"
+)
+
+type RAG struct {
+	g *genkit.Genkit
+	q *db.Queries
+	e ai.Embedder
+}
+
+func New(ctx context.Context, q *db.Queries) RAG {
+	// Init genkit
+	g, err := genkit.Init(ctx)
+	if err != nil {
+		log.Fatalf("could not initialize Genkit: %v", err)
+	}
+
+	// Fail fast if prompt wasn't found
+	p := genkit.LookupPrompt(g, "logseq-cozo")
+	if p == nil {
+		log.Fatal("no prompt named 'menu' found")
+	}
+
+	// Setup OpenAI embedder
+	oaiKey := os.Getenv("OPENAI_API_TOKEN")
+	if oaiKey == "" {
+		log.Fatal("OPENAI_API_TOKEN env var should be set")
+	}
+	oai := openai.OpenAI{
+		APIKey: oaiKey,
+	}
+	if err = oai.Init(ctx, g); err != nil {
+		log.Fatalf("failed to init OpenAI plugin with %s", err)
+	}
+	embedder, err := oai.DefineEmbedder(g, "text-embedding-3-small")
+	if err != nil {
+		log.Fatalf("failed to define OpenAI embedder with %s", err)
+	}
+
+	// Done
+	return RAG{
+		g: g,
+		q: q,
+		e: embedder,
+	}
+}
+
+func (r RAG) Index(query string) error {
+	panic("not implemented")
+}
+
+func (r RAG) Retrieve(query string) (*ai.RetrieverResponse, error) {
+	panic("not implemented")
+}
