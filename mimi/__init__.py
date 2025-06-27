2ed34df9279c260a1cbe3e3e296a269abc47149a
scipunch <scipunch@gmail.com>
2025-06-27 20:56:39 +0200

    chore: Remove python code

--- mimi/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data_sink import DataSink
-from .domain import DataOrigin
-
-__all__ = ["DataOrigin", "DataSink"]
--- mimi/data_sink.py
+++
@@ -1,6 +0,0 @@
-from typing import Protocol
-
-
-class DataSink[T](Protocol):
-    def put(self, obj: T) -> None: ...
-    def get(self) -> T: ...
