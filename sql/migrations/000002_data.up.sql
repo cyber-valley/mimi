826fd99d15bda6fad4a6c9ccf25b49b079e19571
scipunch <scipunch@gmail.com>
2025-06-28 23:32:28 +0200

    chore: Remove wrong chat

--- sql/migrations/000002_data.up.sql
+++ sql/migrations/000002_data.up.sql
@@ -1,7 +1,6 @@
 INSERT INTO
     telegram_peer (chat_name, id)
 VALUES
-    ('cyber devops force', 2572439595),
     ('cyber devops force', 4588050232),
     ('rockets', 2245058785),
     ('cyber valley core', 4104563751);
