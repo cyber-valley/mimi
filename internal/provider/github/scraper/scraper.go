f59035ed33a247eb3deedc5a04c72470337f42e5
scipunch <scipunch@gmail.com>
2025-07-03 02:14:24 +0200

    add: partial github scraper refactor

--- internal/provider/github/scraper/scraper.go
+++ internal/provider/github/scraper/scraper.go
@@ -1,36 +1,64 @@
 package scraper
 
 import (
-	"errors"
 	"fmt"
+	"log/slog"
 	"net/http"
 	"os"
+	"slices"
 
 	"github.com/golang/glog"
 	"github.com/google/go-github/v72/github"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"mimi/internal/persist"
 )
 
-type eventMonitor struct {
-	webhookSecretKey []byte
+const (
+	githubWebhookSecretEnv = "GITHUB_WEBHOOK_SECRET"
+	baseRepositoryPathEnv  = "GITHUB_REPOSITORY_BASE_PATH"
+)
+
+type webhookHandler struct {
+	webhookSecretKey   []byte
+	db                 *pgxpool.Pool
+	baseRepositoryPath string
 }
 
-func Run(port int) error {
+func Run(port int, db *pgxpool.Pool) error {
 	glog.Info("Setting up")
-	pk := os.Getenv("GITHUB_WEBHOOK_SECRET")
+	// Load environment
+	pk := os.Getenv(githubWebhookSecretEnv)
 	if pk == "" {
-		return errors.New("missing GITHUB_WEBHOOK_SECRET env variable")
+		return fmt.Errorf("missing %s env variable", githubWebhookSecretEnv)
 	}
-	m := eventMonitor{
-		webhookSecretKey: []byte(pk),
+	basePath := os.Getenv(baseRepositoryPathEnv)
+	if basePath == "" {
+		return fmt.Errorf("missing %s env variable", baseRepositoryPathEnv)
+	}
+
+	// Create base repository path does not exist
+	err := os.MkdirAll(basePath, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to create base GitHub repository path with %w", err)
+	}
+
+	// Setup multiplexer
+	h := webhookHandler{
+		webhookSecretKey:   []byte(pk),
+		db:                 db,
+		baseRepositoryPath: basePath,
 	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("POST /github/webhook", m.handleWebhook)
+	mux.HandleFunc("POST /github/webhook", h.handleWebhook)
 	glog.Info("Starting")
+
+	// Serve
 	return http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
 }
 
-func (m eventMonitor) handleWebhook(w http.ResponseWriter, r *http.Request) {
-	payload, err := github.ValidatePayload(r, m.webhookSecretKey)
+func (h webhookHandler) handleWebhook(w http.ResponseWriter, r *http.Request) {
+	payload, err := github.ValidatePayload(r, h.webhookSecretKey)
 	if err != nil {
 		glog.Errorf("signature validation %s", err)
 		http.Error(w, err.Error(), http.StatusUnauthorized)
@@ -44,17 +72,20 @@ func (m eventMonitor) handleWebhook(w http.ResponseWriter, r *http.Request) {
 	switch event := event.(type) {
 	case *github.PushEvent:
 		glog.Infof("Got push event %#v", event)
-	case *github.IssueEvent:
-		glog.Infof("Got issue event %#v", event)
-	case *github.IssueCommentEvent:
-		glog.Infof("Got issue comment event %#v", event)
-	case *github.ProjectV2Event:
-		glog.Infof("Got project event %#v", event)
-	case *github.ProjectV2ItemEvent:
-		glog.Infof("Got project item event %#v", event)
+		q := persist.New(h.db)
+		repos, err := q.FindGitHubRepositories(r.Context())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotImplemented)
+			return
+		}
+		watched := slices.ContainsFunc(repos, func(r persist.GithubRepository) bool {
+			return r.Name == *event.Repo.Name && r.Owner == *event.Repo.Owner.Login
+		})
+		if !watched {
+			slog.Warn("got push to unwatched GitHub repository", "event", event)
+		}
 	default:
 		glog.Warning("Got unexpected event %#v", event)
-		http.Error(w, "", http.StatusNotImplemented)
 	}
 	w.WriteHeader(http.StatusCreated)
 }
