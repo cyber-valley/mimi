ea81a6ed074c8e53c6ac379349f3b5a88f5858d0
scipunch <scipunch@gmail.com>
2025-07-04 22:30:06 +0200

    feat: Retrieve summary data in parallel

--- internal/bot/llm/agent/summary/summary.go
+++ internal/bot/llm/agent/summary/summary.go
@@ -3,10 +3,12 @@ package summary
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"log/slog"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/firebase/genkit/go/ai"
@@ -63,7 +65,6 @@ func (a SummaryAgent) GetInfo() agent.Info {
 	}
 }
 
-// TODO: Most of the retrieving could be executed in parallel
 func (a SummaryAgent) Run(ctx context.Context, query string, msgs ...*ai.Message) (*ai.ModelResponse, error) {
 	resp, err := a.periodExtractor.Execute(ctx, ai.WithInput(map[string]any{"query": query}))
 	if err != nil {
@@ -83,52 +84,91 @@ func (a SummaryAgent) Run(ctx context.Context, query string, msgs ...*ai.Message
 	case "day":
 		since = since.AddDate(0, 0, -1)
 	}
-	var docs []*ai.Document
+	docChan := make(chan *ai.Document, 3)
+	errChan := make(chan error, 3)
+	var wg sync.WaitGroup
+	wg.Add(3)
+	startT := time.Now()
 
 	// Retrieve GitHub projects statuses
-	issues := make(map[string][]db.Issue)
-	// TODO: Should it be persisted in DB?
-	projects := map[string]int{
-		"rockets":      2,
-		"supply":       3,
-		"inventory":    24,
-		"devops force": 33,
-	}
-	// Fetch issues for each project
-	for title, projID := range projects {
-		tmp, err := a.ghClient.GetOrgProject(ctx, a.ghOrg, projID, since)
-		if err != nil {
-			return nil, fmt.Errorf("failed to fetch supply board state with %w", err)
+	go func() {
+		defer wg.Done()
+		issues := make(map[string][]db.Issue)
+		// TODO: Should it be persisted in DB?
+		projects := map[string]int{
+			"rockets":      2,
+			"supply":       3,
+			"inventory":    24,
+			"devops force": 33,
 		}
-		slog.Info("fetched GitHub issues", "project", title, "lenght", len(tmp), "value", tmp)
-		issues[title] = tmp
-	}
-	blob, err := json.Marshal(issues)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal GitHub projects info with %w", err)
-	}
-	docs = append(docs, ai.DocumentFromText(string(blob), map[string]any{"info": "GitHub projects issues"}))
+
+		// Fetch issues for each project
+		for title, projID := range projects {
+			tmp, err := a.ghClient.GetOrgProject(ctx, a.ghOrg, projID, since)
+			if err != nil {
+				errChan <- fmt.Errorf("failed to fetch supply board state with %w", err)
+				return
+			}
+			slog.Info("fetched GitHub issues", "project", title, "lenght", len(tmp), "value", tmp)
+			issues[title] = tmp
+		}
+		blob, err := json.Marshal(issues)
+		if err != nil {
+			errChan <- fmt.Errorf("failed to marshal GitHub projects info with %w", err)
+			return
+		}
+		docChan <- ai.DocumentFromText(string(blob), map[string]any{"info": "GitHub projects issues"})
+	}()
 
 	// Retrieve Telegram info
-	q := persist.New(a.pgPool)
-	messages, err := q.FindTelegramMessages(ctx, pgtype.Timestamptz{Time: since, Valid: true})
-	if err != nil {
-		return nil, fmt.Errorf("failed to retrieve Telegram message from DB with %w", err)
-	}
-	slog.Info("retrieved Telegram messages", "length", len(messages))
-	blob, err = json.Marshal(messages)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal Telegram messages with %w", err)
-	}
-	docs = append(docs, ai.DocumentFromText(string(blob), map[string]any{"info": "Related telegram messages"}))
+	go func() {
+		defer wg.Done()
+		q := persist.New(a.pgPool)
+		messages, err := q.FindTelegramMessages(ctx, pgtype.Timestamptz{Time: since, Valid: true})
+		if err != nil {
+			errChan <- fmt.Errorf("failed to retrieve Telegram message from DB with %w", err)
+			return
+		}
+		slog.Info("retrieved Telegram messages", "length", len(messages))
+		blob, err := json.Marshal(messages)
+		if err != nil {
+			errChan <- fmt.Errorf("failed to marshal Telegram messages with %w", err)
+			return
+		}
+		docChan <- ai.DocumentFromText(string(blob), map[string]any{"info": "Related telegram messages"})
+	}()
 
 	// Retrieve LogSeq diff
-	diff, err := git.DiffInterval(a.logseqRepoPath, since)
-	if err != nil {
-		return nil, err
+	go func() {
+		defer wg.Done()
+		diff, err := git.DiffInterval(a.logseqRepoPath, since)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		slog.Info("retrieved LogSeq diff", "length", len(diff))
+		docChan <- ai.DocumentFromText(diff, map[string]any{"info": "LogSeq git diff"})
+	}()
+
+	wg.Wait()
+	slog.Info("summary data retrieved", "elapsed", time.Since(startT))
+	close(errChan)
+	close(docChan)
+
+	// Process errors
+	var errs []error
+	for err := range errChan {
+		errs = append(errs, err)
 	}
-	slog.Info("retrieved LogSeq diff", "length", len(diff))
-	docs = append(docs, ai.DocumentFromText(diff, map[string]any{"info": "LogSeq git diff"}))
+	if len(errs) > 0 {
+		return nil, fmt.Errorf("failed to retrieve data for summary with %w", errors.Join(errs...))
+	}
+
+	// Call LLM
+	var docs []*ai.Document
+	for doc := range docChan {
+		docs = append(docs, doc)
+	}
 
 	resp, err = a.evalPrompt.Execute(ctx, ai.WithDocs(docs...), ai.WithInput(map[string]any{"period": period}))
 	if err != nil {
